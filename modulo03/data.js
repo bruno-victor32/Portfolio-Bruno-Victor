9f09d19cd208f77ab42135258b8418703f2ebbd8
bruno-victor32 <bruno.victor32@yahoo.com.br>
2020-07-26 16:16:29 -0300

    Primeiro Commit

---
+++ modulo03/data.js
@@ -0,0 +1,42 @@
+/*---Arquivos de dados---*/
+module.exports = [/*Aqui estou exportando um array*/
+    {
+        id : "7qqtLkl268c", 
+        title: "Engenharia e fácil? | Bruno Victor",
+        duration:"07 min",
+        price:"Free",
+        featured: true
+    },
+    {
+        id : "TneQ8eIY454", /*Segundo Objeto*/
+        title: "O que vai rolar por aqui? |Thaissa Candela",
+        duration:"03 min",
+        price:"Free"
+    },
+    {
+        id : "oUPaJxk6TZ0",/*Terceiro Objeto*/
+        title: "Guia definitivo de aprendendo a aprender | Fabio Akita",
+        duration:"1 hora 04 min",
+        price:"Free"
+    },
+    {
+        id : "Ud-Z3vrJKTk",/*Quarto Objeto*/
+        title: "Falha grave da uol permite invasão por um simples envio de E-mail | Gabriel Pato",
+        duration:"19 min",
+        price:"Free",
+        featured: true
+    },
+    {
+        id : "-u4afrfBsTE",/*Quinto Objeto*/
+        title: "Keep At Bay O que significa esta expressão? | Mairo Vergara",
+        duration:"07 min",
+        price:"Free"
+    },
+    {
+        id : "q6ppzNXcA0U",/*Sexto Objeto*/
+        title: "Comprei um mini computador | Edukof",
+        duration:"16 min",
+        price:"Free",
+        featured: true
+    }
+]
